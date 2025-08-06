4d725b5c23d8be2ab00164ad3fac90447b28ed32
NKovp <kovpakdev@gmail.com>
2025-08-06 22:51:39 +0100

    Create initial Web API project

---
+++ Source/create_pokemon_db.sql
@@ -0,0 +1,8 @@
+-- PostgreSQL Database Creation Script
+-- Run this first to create the database, then run pokemon_database_complete.sql
+
+-- Create database
+CREATE DATABASE pokemon_db;
+
+-- Connect to the database (you'll need to do this manually)
+-- \c pokemon_db;
